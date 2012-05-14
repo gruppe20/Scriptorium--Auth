efe896744823d3059c591825afa016bcdb149bb3
Ole Grinnby <ogrinnby@gmail.com>
2012-05-14 13:48:26 +0200

    Added new callback path - hardcoded

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,8 +2,12 @@ require 'omniauth-openid'
 require 'openid/store/filesystem'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
+  #OmniAuth.config.full_host = "localhost:3000/login"
+
+
   provider :open_id, 
             :store => OpenID::Store::Filesystem.new('/tmp'), 
             :require => 'omniauth-openid', 
+            :callback_path => '/login/auth/open_id/callback',
             :identifier => 'pc29-75.iu.hio.no/openid'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Auth::Engine.routes.draw do
   get "home/index"
+  #get "/auth/open_id/callback" => "sessions#create", :as => :callback
 
   root :to => "home#index"
 
-  match "auth/open_id/callback" => "sessions#create"#, :as => :callback
+  match "/auth/open_id/callback" => "sessions#create", :as => :callback
 end
